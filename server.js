c7c1cc2df17913185bf733acd9ff13c960c1e56e
paul <paul@b7interactive.com>
2015-08-10 12:48:20 -0600

    more cors changes

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
-app.use(function(req, res){
-    res.setHeader("Access-Control-Allow-Origin", "http://pdubs.gyges.feralhosting.com");
-}
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // routes ======================================================================
 require('./app/routes.js')(app);
