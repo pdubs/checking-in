dcaf3acf96e6f2e9ada0a3f6aadbdc881f311530
paul <paul@b7interactive.com>
2015-08-10 12:28:36 -0600

    changing port

--- public/js/services/checkins.js
+++ public/js/services/checkins.js
@@ -5,13 +5,13 @@ angular.module('checkinService', [])
 	.factory('Checkins', ['$http',function($http) {
 		return {
 			get : function() {
-				return $http.get('/api/checkins');
+				return $http.get('http://localhost:8095/api/checkins');
 			},
 			create : function(checkinData) {
-				return $http.post('/api/checkins', checkinData);
+				return $http.post('http://localhost:8095/api/checkins/', checkinData);
 			},
 			delete : function(id) {
-				return $http.delete('/api/checkins/' + id);
+				return $http.delete('http://localhost:8095/api/checkins/' + id);
 			}
 		}
 	}]);
